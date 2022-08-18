37d2773617bb4d4d0bf58549896ad05f9a645af4
Eren Taşçı <erent4sci@gmail.com>
2022-08-18 17:45:11 +0300

    Added 03-reviews

---
+++ 03-reviews/app.js
@@ -0,0 +1,106 @@
+//Reviews Data
+const reviews = [
+  {
+    id: 1,
+    name: "susan smith",
+    job: "web developer",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg",
+    text:
+      "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+  },
+  {
+    id: 2,
+    name: "anna johnson",
+    job: "web designer",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg",
+    text:
+      "Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.",
+  },
+  {
+    id: 3,
+    name: "peter jones",
+    job: "intern",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg",
+    text:
+      "Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.",
+  },
+  {
+    id: 4,
+    name: "bill anderson",
+    job: "the boss",
+    img:
+      "https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg",
+    text:
+      "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
+  },
+];
+
+
+// Select items
+
+const img = document.getElementById("person-img");
+const author = document.getElementById("author");
+const job = document.getElementById("job");
+const info = document.getElementById("info");
+
+// Select buttons
+  
+   // Select prev. button
+const prevBtn = document.querySelector(".prev-btn");
+
+   // Select next button
+const nextBtn = document.querySelector(".next-btn");
+
+   // Select random button
+const randomBtn = document.querySelector(".random-btn");   
+
+
+//set current item
+let currentItem = 0;
+
+
+function showPerson(currentItem){
+
+  let item = reviews[currentItem];
+
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+
+}
+
+// When screen loaded,show person
+
+window.addEventListener("DOMContentLoaded", () => {
+  showPerson(currentItem);
+})
+
+// Set next button func.
+nextBtn.addEventListener("click", () => {
+  currentItem++;
+  if(currentItem>reviews.length-1){
+    currentItem=0;
+  }
+
+  showPerson(currentItem);
+} )
+
+// Set prev button func.
+prevBtn.addEventListener("click", () => {
+  currentItem--;
+  if(currentItem<0){
+    currentItem= (reviews.length-1);
+  }
+  showPerson(currentItem);
+} )
+
+// Set random button func.
+randomBtn.addEventListener("click", () => {
+  currentItem = Math.floor((Math.random() * reviews.length));
+  console.log(currentItem);
+  showPerson(currentItem);
+})
